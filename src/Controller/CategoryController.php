7d17d3539297ab21831e9d2abcc2ae6bf5bcfc83
Mrbilal <bilal_itchir@Outlook.fr>
2024-01-18 16:45:19 +0100

    🐛 corriger(CategoryController.php) : déplacer l'importation de l'exception FileException pour éviter une erreur de compilation ✨ fonctionnalité(CategoryController.php) : ajouter la fonctionnalité d'édition d'une catégorie avec un formulaire ✨ fonctionnalité(CategoryController.php) : ajouter la fonctionnalité de création d'une nouvelle catégorie avec un formulaire 🐛 corriger(CategoryType.php) : supprimer les contraintes de validation du champ image pour éviter une erreur de validation

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,57 +5,58 @@ namespace App\Controller;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class CategoryController extends AbstractController
 {
     #[Route('/{category}/edit', name: 'category_edit')]
     public function edit(
-        // Inject the request object to get the category name
-        Request                $request,
-        // Inject the category repository to find the category
-        CategoryRepository     $categoryRepository,
-        // Inject the post repository to find all posts in the category
-        EntityManagerInterface $em
-    ): Response
-    {
-        // Find the category by its name
+        Request $request,
+        CategoryRepository $categoryRepository,
+        EntityManagerInterface $em // To save the category edited
+    ): Response {
         $category = $categoryRepository->findOneBy([
             'name' => $request->get('category')
         ]);
 
-        // TODO Add the form here
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request); // Handle the request
+        // Edit form + proccess
+        $form = $this->createForm(CategoryType::class);
+        $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             if ($form->get('image')->getData()) {
                 $imageFile = $form->get('image')->getData();
                 $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
-                $newFilename = $originalFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+                $newFilename = $originalFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
                 try {
                     $imageFile->move(
                         $this->getParameter('uploads_directory'),
                         $newFilename
                     );
-                    $category->setImage($newFilename); // Set the new filename in the category
+                    $category->setImage($newFilename);
                 } catch (FileException $e) {
-                    $this->addFlash('danger', 'Une erreur est survenue lors de l\'upload de votre Image.');
+                    $this->addFlash('danger', 'Une erreur est survenue lors de l\'upload de votre image');
                 }
             }
+
             $category->setName($form->get('name')->getData());
-            // Save the category
             $em->persist($category);
             $em->flush();
-            return $this->redirectToRoute('category', ['category' => $category->getName()]);
+
+            // Redirect to the category page
+            return $this->redirectToRoute('category', [
+                'category' => $category->getName()
+            ]);
         }
+
+
         // Return the view
         return $this->render('category/edit.html.twig', [
             // Pass the category object to the view
-            'title' => 'Modifier la categorie ' . $category->getName(),
             'category' => $category,
             'editForm' => $form
         ]);
@@ -65,11 +66,10 @@ class CategoryController extends AbstractController
     #[Route('/new-category', name: 'category_new')]
     public function new(
         // Inject the request object to get the data from the form
-        Request                $request,
+        Request $request,
         // Add the EntityManagerInterface to save the category
         EntityManagerInterface $em,
-    ): Response
-    {
+    ): Response {
         // TODO Add the form here
 
         // TODO Add the form proccess here
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,34 +4,24 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, ['required' => true])
+            ->add('name', TextType::class)
             ->add('image', FileType::class, [
                 'mapped' => false,
-                'constraints' => [
-                    new File([
-                        'maxSize' => '1024k',
-                        'mimeTypes' => [
-                            'image/jpeg',
-                            'image/png',
-                        ],
-                        'mimeTypesMessage' => 'Veuillez uploader un fichier Image valide',
-                    ])],
                 'required' => false
             ])
-            ->add('Enregistrer', SubmitType::class) //->add('submit', SubmitType::class, ['label' => 'Modifier'])
-    ;
+            ->add('Enregistrer', SubmitType::class)
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
