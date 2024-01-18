5b201e7351d59cb1a5c02b40e73dcb66d586c3b4
Mrbilal <bilal_itchir@Outlook.fr>
2024-01-18 16:18:17 +0100

    📝 ajout(CategoryType.php) : ajouter un formulaire pour la création de catégories avec les champs nom et image 📝 ajout(PostType.php) : ajouter un formulaire pour la création de publications avec les champs titre, extrait, contenu, image, est publié et catégorie

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, ['required' => true])
+            ->add('image', FileType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez uploader un fichier Image valide',
+                    ])],
+                'required' => false
+            ])
+            ->add('Enregistrer', SubmitType::class) //->add('submit', SubmitType::class, ['label' => 'Modifier'])
+    ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
