82fbc0e3fc861d0b4a2faf07b9893bb62d0cad7e
Mrbilal <bilal_itchir@Outlook.fr>
2024-01-18 16:46:11 +0100

    🐛 corriger(PageController.php) : réduire la limite par page de 10 à 9 ✨ fonctionnalité(PageController.php) : modifier le chemin de la route pour afficher un article individuel 🐛 corriger(PageController.php) : supprimer les commentaires redondants 🐛 corriger(PageController.php) : supprimer les commentaires redondants 🐛 corriger(UserRepository.php) : ajouter une nouvelle ligne vide à la fin du fichier

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -24,7 +24,7 @@ class PageController extends AbstractController
         $posts = $paginator->paginate(
             $postRepository->findAll(), // Request
             $request->query->getInt('page', 1), // Page number
-            10 // Limit per page
+            9 // Limit per page
         );
         return $this->render('page/home.html.twig', [
             'posts' => $posts,
@@ -32,8 +32,7 @@ class PageController extends AbstractController
         ]);
     }
 
-    //
-    #[Route('/{slug}', name: 'post', methods: ['GET'])]
+    #[Route('/post/{slug}', name: 'post', methods: ['GET'])]
     public function post(
         // Inject the request object to get the category name (NE PAS REECRIRE TOUS LES COMMENTAIRES)
         Request $request,
@@ -56,28 +55,17 @@ class PageController extends AbstractController
     // Route for displaying a single category (NE PAS REECRIRE TOUS LES COMMENTAIRES)
     #[Route('/{category}', name: 'category', methods: ['GET'])]
     public function category(
-        // Inject the request object to get the category name (NE PAS REECRIRE TOUS LES COMMENTAIRES)
         Request $request,
-        // Inject the category repository to find the category
         CategoryRepository $categoryRepository,
-        // Inject the post repository to find all posts in the category
         PostRepository $postRepository,
     ): Response {
-        // Find the category by its name
         $category = $categoryRepository->findOneBy([
             'name' => $request->get('category')
         ]);
-        // Return the view
         return $this->render('page/category.html.twig', [
-            // Pass the category object to the view
             'category' => $category,
-            // Pass all posts in the category to the view
-            'posts' => $postRepository->findBy(
-                [
-                    'category' => $category
-                ]
+            'posts' => $postRepository->findBy(['category' => $category]
             )
         ]);
     }
-
 }
